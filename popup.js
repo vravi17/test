95cc19da24d89daf1383dc78f773a988777b05e4
vravi17 <vishaakravi17@gmail.com>
2015-09-26 01:48:05 -0400

    hi

---
+++ popup.js
@@ -0,0 +1,11 @@
+function loadUrls() {
+	
+}
+
+function saveUrls() {
+	
+}
+
+document.getElementById('button').addEventListener('click',loadUrls);
+
+document.getElementById('button').addEventListener('click',saveUrls);
